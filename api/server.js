1471facf29e8c90573316256f90216d11e30827a
Chevy <75184977+chavionjackson@users.noreply.github.com>
2021-10-01 09:43:14 -0700

    get by id done

--- api/server.js
+++ api/server.js
@@ -20,4 +20,21 @@ server.get("/api/posts", (req, res) => {
     });
 });
 
-module.exports = server
+server.get("/api/posts/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then((post) => {
+      post
+        ? res.status(200).json(post)
+        : res.status(404).json({
+            message: "The post with the specified ID does not exist",
+          });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "The post information could not be retrieved",
+        err: err.message,
+      });
+    });
+});
+
+module.exports = server;
