a95210c12acbdd7270f7421bb3f5b5180ff20a68
Chevy <75184977+chavionjackson@users.noreply.github.com>
2021-10-01 09:36:10 -0700

    get posts done

--- api/server.js
+++ api/server.js
@@ -1,2 +1,23 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require("express");
+const server = express();
+const Post = require("./posts/posts-model");
+
+server.use(express.json());
+
+server.get("/api/posts", (req, res) => {
+  Post.find()
+    .then((post) => {
+      console.log(post);
+      res.status(200).json(post);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "The posts information could not be retrieved",
+        err: err.message,
+      });
+    });
+});
+
+module.exports = server
