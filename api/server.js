3041138bbb01a1b063bbe4981cb48ba44c0d1307
Chevy <75184977+chavionjackson@users.noreply.github.com>
2021-10-01 14:45:26 -0700

    get comments done

--- api/server.js
+++ api/server.js
@@ -108,4 +108,25 @@ server.delete("/api/posts/:id", async (req, res) => {
   }
 });
 
+server.get("/api/posts/:id/comments", async (req, res) => {
+  const { id } = req.params;
+  const post = await Post.findById(id);
+  if (!post) {
+    res
+      .status(404)
+      .json({ message: "The post with the specified ID does not exist" });
+  } else {
+    try {
+      const comments = await Post.findPostComments(id);
+      console.log(comments);
+      res.status(200).json(comments);
+    } catch (err) {
+      res
+        .status(500)
+        .json({ message: "The comments information could not be retrieved" });
+    }
+  }
+});
+
+
 module.exports = server;
