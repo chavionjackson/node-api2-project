6e5682e8e50738869193c0168cec9e5234b6ec7d
Chevy <75184977+chavionjackson@users.noreply.github.com>
2021-10-01 14:31:20 -0700

    put done

--- api/server.js
+++ api/server.js
@@ -60,4 +60,35 @@ server.post("/api/posts", (req, res) => {
         });
 });
 
+server.put("/api/posts/:id", async (req, res) => {
+  const changes = req.body;
+  const { id } = req.params;
+  const post = await Post.findById(id);
+  try {
+    if (!post) {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist" });
+    } else if (!changes.title || !changes.contents) {
+      res
+        .status(400)
+        .json({ message: "Please provide title and contents for the post" });
+    } else {
+      await Post.update(id, changes);
+
+      res.status(200).json({
+        id: parseInt(id),
+        title: changes.title,
+        contents: changes.contents,
+      });
+    }
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "The post information could not be modified" });
+  }
+});
+
+
+
 module.exports = server;
