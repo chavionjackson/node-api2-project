57b37291a24c70c1f73f7f6037216fe0bb97c4aa
Chevy <75184977+chavionjackson@users.noreply.github.com>
2021-10-01 14:43:19 -0700

    delete done

--- api/server.js
+++ api/server.js
@@ -89,6 +89,23 @@ server.put("/api/posts/:id", async (req, res) => {
   }
 });
 
-
+server.delete("/api/posts/:id", async (req, res) => {
+  try {
+    const deletedPost = await Post.findById(req.params.id);
+    if (!deletedPost) {
+      res.status(404).json({
+        message: "The post with the specified ID does not exist",
+      });
+    } else {
+      Post.remove(req.params.id).then(() => {
+        res.status(200).json(deletedPost);
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "The post could not be removed",
+    });
+  }
+});
 
 module.exports = server;
